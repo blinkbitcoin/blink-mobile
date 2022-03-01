1a20900ca99a8c4b35035602ddb7ae5df56aaede
Lee Salminen <leesalminen@gmail.com>
2022-03-01 15:00:53 -0600

    update whatsapp phone number to whatsapp for business

--- app/constants/support.ts
+++ app/constants/support.ts
@@ -1,3 +1,3 @@
-export const WHATSAPP_CONTACT_NUMBER = "+50687314078"
+export const WHATSAPP_CONTACT_NUMBER = "+19704505406"
 export const GALOY_PAY_DOMAIN = "https://pay.bitcoinjungle.app/"
 export const BLOCKCHAIN_EXPLORER_URL = "https://mempool.space/tx/"
