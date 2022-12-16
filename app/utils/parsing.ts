0084228be9b16422cfc99ea6ab6a5520665ff9e2
Lee Salminen <leesalminen@gmail.com>
2022-12-16 16:53:09 -0600

    add taproot support

--- app/utils/parsing.ts
+++ app/utils/parsing.ts
@@ -167,8 +167,14 @@ export const validPayment = (
         console.log(`can't decode amount ${err}`)
       }
 
-      // will throw if address is not valid
-      address.toOutputScript(path, mappingToBitcoinJs(network))
+      // FIXME "toOutputScript" will throw if address is not valid, however bitcoinjs-lib does not currently support
+      // taproot addresses so we must bypass this check if the address uese taproot
+      const isTaprootAddress = lowerCasePath.startsWith("bc1p")
+      if(!isTaprootAddress) {
+        // will throw if address is not valid
+        address.toOutputScript(path, mappingToBitcoinJs(network))
+      }
+      
       paymentType = "onchain"
       return {
         valid: true,
