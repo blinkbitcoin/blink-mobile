b109da8c937c01e0849228ce41401e24aa9a6ca3
JOSE MOISES NUÑEZ IGLESIAS <numezmoises1@gmail.com>
2025-06-03 09:50:38 -0600

    fix: update token for the previously saved session

--- app/hooks/use-save-session-profile.ts
+++ app/hooks/use-save-session-profile.ts
@@ -85,10 +85,18 @@ export const useSaveSessionProfile = () => {
       if (profile.accountId) setUpgradeModalShown(client, false)
 
       const exists = profiles.some((p) => p.accountId === profile.accountId)
+      const cleaned = profiles.map((p) => ({ ...p, selected: false }))
       if (!exists) {
-        const cleaned = profiles.map((p) => ({ ...p, selected: false }))
         await KeyStoreWrapper.saveSessionProfiles([{ ...profile }, ...cleaned])
+        return
       }
+
+      // Update token for the previously saved session
+      const updatedProfiles = cleaned.map((p) =>
+        p.accountId === profile.accountId ? { ...p, token: profile.token } : p,
+      )
+
+      await KeyStoreWrapper.saveSessionProfiles(updatedProfiles)
     },
     [saveToken, tryFetchUserProps, fetchUsername, client],
   )
