a8b234071fed5c7ef008fd6c17cae003865e29e5
Juan P Lopez <dolcalmi@gmail.com>
2025-09-04 22:30:26 -0500

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -89,15 +89,8 @@
                 emulator
                 platform-tools
                 platforms-android-35
-                ndk-25-1-8937393
                 ndk-26-1-10909125
                 cmake-3-22-1
-
-                # Some dependencies we use are on old versions
-                # TODO: Update these obsolete dependencies
-                build-tools-30-0-3
-                build-tools-33-0-1
-                platforms-android-33
               ]
               ++ lib.optionals (system == "aarch64-darwin") [
                 system-images-android-35-google-apis-arm64-v8a
