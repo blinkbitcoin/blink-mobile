97431e4630454a873b25a8ce37bfa0ab56f23cf8
JOSE MOISES NUÑEZ IGLESIAS <numezmoises1@gmail.com>
2025-11-04 15:12:28 -0600

    chore: delete account level from settign

--- app/screens/settings-screen/settings/account-level.tsx
+++ app/screens/settings-screen/settings/account-level.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { useLevel } from "@app/graphql/level-context"
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { RootStackParamList } from "@app/navigation/stack-param-lists"
 import { useNavigation } from "@react-navigation/native"
@@ -8,13 +7,11 @@ import { StackNavigationProp } from "@react-navigation/stack"
 import { SettingsRow } from "../row"
 
 export const AccountLevelSetting: React.FC = () => {
-  const { currentLevel: level } = useLevel()
   const { LL } = useI18nContext()
   const { navigate } = useNavigation<StackNavigationProp<RootStackParamList>>()
   return (
     <SettingsRow
       title={LL.common.account()}
-      subtitle={LL.AccountScreen.level({ level })}
       leftIcon="person"
       action={() => {
         navigate("accountScreen")
