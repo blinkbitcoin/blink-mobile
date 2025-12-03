b191a9bddf0e495663f539170004b9a7baf347b1
Juan P Lopez <dolcalmi@gmail.com>
2025-12-03 00:30:12 -0500

    chore: update account cache on pull to refresh (#3546)

--- app/screens/settings-screen/account/account-screen.tsx
+++ app/screens/settings-screen/account/account-screen.tsx
@@ -1,4 +1,5 @@
-import { ScrollView } from "react-native-gesture-handler"
+import React, { useState, useCallback } from "react"
+import { ScrollView, RefreshControl } from "react-native-gesture-handler"
 
 import { Screen } from "@app/components/screen"
 import { testProps } from "@app/utils/testProps"
@@ -11,15 +12,27 @@ import { DangerZoneSettings } from "./settings/danger-zone"
 import { UpgradeAccountLevelOne } from "./settings/upgrade"
 import { UpgradeTrialAccount } from "./settings/upgrade-trial-account"
 import { SettingsGroup } from "../group"
+import { useSaveSessionProfile } from "@app/hooks/use-save-session-profile"
 
 export const AccountScreen: React.FC = () => {
   const styles = useStyles()
+  const { updateCurrentProfile } = useSaveSessionProfile()
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true)
+    await updateCurrentProfile()
+    setRefreshing(false)
+  }, [updateCurrentProfile])
 
   return (
     <AccountDeleteContextProvider>
       <Screen keyboardShouldPersistTaps="handled">
         <ScrollView
           contentContainerStyle={styles.outer}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
           {...testProps("account-screen-scroll-view")}
         >
           <AccountBanner />
