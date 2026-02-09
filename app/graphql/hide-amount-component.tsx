dac83274091ae8ec3ff652b83057b1d298a25549
designsats <andrejcibik@gmail.com>
2026-02-09 15:51:22 -0600

    Hide balance persists

--- app/graphql/hide-amount-component.tsx
+++ app/graphql/hide-amount-component.tsx
@@ -1,16 +1,26 @@
 import * as React from "react"
-import { PropsWithChildren, useState } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 
+import { useApolloClient } from "@apollo/client"
 import { useHideBalanceQuery } from "@app/graphql/generated"
 
+import { saveHiddenBalanceToolTip, saveHideBalance } from "./client-only-query"
 import { HideAmountContextProvider } from "./hide-amount-context"
 
 export const HideAmountContainer: React.FC<PropsWithChildren> = ({ children }) => {
+  const client = useApolloClient()
   const { data: { hideBalance } = { hideBalance: false } } = useHideBalanceQuery()
   const [hideAmount, setHideAmount] = useState(hideBalance)
 
+  useEffect(() => {
+    setHideAmount(hideBalance)
+  }, [hideBalance])
+
   const switchMemoryHideAmount = () => {
-    setHideAmount(!hideAmount)
+    const shouldHideBalance = !hideAmount
+    setHideAmount(shouldHideBalance)
+    saveHideBalance(client, shouldHideBalance)
+    saveHiddenBalanceToolTip(client, shouldHideBalance)
   }
 
   return (
