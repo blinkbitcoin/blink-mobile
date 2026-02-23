cfc2afba2592b105e1edbeec863f1755fae3bf81
theamodhshetty <theamodhshetty@gmail.com>
2026-02-23 12:14:15 +0530

    fix: support bitcoin and lightning links without //

---
+++ app/navigation/linking-prefixes.ts
@@ -0,0 +1,10 @@
+export const COMPATIBLE_LINK_PREFIXES = [
+  "bitcoin://",
+  "bitcoin:",
+  "lightning://",
+  "lightning:",
+  "lapp://",
+  "lnurlw://",
+  "lnurlp://",
+  "lnurl://",
+] as const
