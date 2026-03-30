56fff1ba86164f9258637440a116c52ac2ec8e72
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-03-30 17:21:49 -0600

    feat(migration): resume from checkpoint in settings entry

--- app/screens/settings-screen/settings/account-move-to-noncustodial.tsx
+++ app/screens/settings-screen/settings/account-move-to-noncustodial.tsx
@@ -5,18 +5,21 @@ import { StackNavigationProp } from "@react-navigation/stack"
 
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { RootStackParamList } from "@app/navigation/stack-param-lists"
+import { useMigrationCheckpoint } from "@app/screens/account-migration/hooks"
 
 import { SettingsRow } from "../row"
 
 export const MoveToNonCustodialSetting: React.FC = () => {
   const { LL } = useI18nContext()
   const { navigate } = useNavigation<StackNavigationProp<RootStackParamList>>()
+  const { loading, getRouteForCheckpoint } = useMigrationCheckpoint()
 
   return (
     <SettingsRow
-      title={LL.SparkMigration.moveToNonCustodial()}
+      title={LL.AccountMigration.moveToNonCustodial()}
       leftGaloyIcon="arrow-right"
-      action={() => navigate("sparkMigrationExplainer")}
+      loading={loading}
+      action={() => navigate(getRouteForCheckpoint())}
     />
   )
 }
