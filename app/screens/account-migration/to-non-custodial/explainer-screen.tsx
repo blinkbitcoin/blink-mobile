76e0e46de651aab6b8a4d90a9dbe7c384887c242
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-04-08 16:55:17 -0600

    refactor: merge explainer i18n keys with inline link tag

--- app/screens/account-migration/to-non-custodial/explainer-screen.tsx
+++ app/screens/account-migration/to-non-custodial/explainer-screen.tsx
@@ -1,38 +1,38 @@
-import React from "react"
-import { Linking, Text } from "react-native"
-
+import React, { useMemo } from "react"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { makeStyles, useTheme } from "@rn-vui/themed"
+import { useTheme } from "@rn-vui/themed"
 
+import { RichText } from "@app/components/rich-text"
+import { useRemoteConfig } from "@app/config/feature-flags-context"
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { RootStackParamList } from "@app/navigation/stack-param-lists"
+import { openExternalUrl } from "@app/utils/external"
 
 import { MigrationExplainerLayout } from "../migration-explainer-layout"
 
-const SPARK_LEARN_MORE_URL = "https://spark.info"
-
 export const SparkMigrationExplainerScreen: React.FC = () => {
   const { LL } = useI18nContext()
-  const styles = useStyles()
   const {
     theme: { colors },
   } = useTheme()
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
-  const handleLearnMore = () => {
-    Linking.openURL(SPARK_LEARN_MORE_URL)
-  }
+  const { sparkCompatibleWalletsUrl } = useRemoteConfig()
 
-  const steps: ReadonlyArray<React.ReactNode> = [
-    <>
-      {LL.AccountMigration.explainerStep1()}
-      <Text style={styles.link} onPress={handleLearnMore}>
-        {LL.AccountMigration.explainerStep1Link()}
-      </Text>
-    </>,
-    LL.AccountMigration.explainerStep2(),
-    LL.AccountMigration.explainerStep3(),
-  ]
+  const steps: ReadonlyArray<React.ReactNode> = useMemo(
+    () => [
+      <RichText
+        key="step1"
+        text={LL.AccountMigration.explainerStep1()}
+        tags={{
+          link: { onPress: () => openExternalUrl(sparkCompatibleWalletsUrl) },
+        }}
+      />,
+      LL.AccountMigration.explainerStep2(),
+      LL.AccountMigration.explainerStep3(),
+    ],
+    [LL, sparkCompatibleWalletsUrl],
+  )
 
   return (
     <MigrationExplainerLayout
@@ -45,10 +45,3 @@ export const SparkMigrationExplainerScreen: React.FC = () => {
     />
   )
 }
-
-const useStyles = makeStyles(({ colors }) => ({
-  link: {
-    textDecorationLine: "underline",
-    color: colors.black,
-  },
-}))
