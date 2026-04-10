f0c7aaa125ec5ab2bc88cce7cc5a9286017f806e
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-04-09 22:47:57 -0600

    feat: add restore method screen

---
+++ app/screens/spark-onboarding/restore/index.ts
@@ -0,0 +1,3 @@
+export { SparkRestorePhraseScreen } from "./restore-phrase-screen"
+export { SparkRestoreMethodScreen } from "./restore-method-screen"
+export { SparkCloudRestoreScreen } from "./cloud-restore-screen"
