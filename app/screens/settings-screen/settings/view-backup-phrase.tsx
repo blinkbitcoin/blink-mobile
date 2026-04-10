216df7eba6c591438dedb9a0b69e2d4616de3071
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-04-09 22:47:35 -0600

    feat: add ViewBackupPhraseSetting component

---
+++ app/screens/settings-screen/settings/view-backup-phrase.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+import { useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
+
+import { useAccountRegistry } from "@app/hooks/use-account-registry"
+import { useI18nContext } from "@app/i18n/i18n-react"
+import { RootStackParamList } from "@app/navigation/stack-param-lists"
+import {
+  BackupStatus,
+  useBackupState,
+} from "@app/self-custodial/providers/backup-state-provider"
+import { AccountType } from "@app/types/wallet.types"
+
+import { SettingsRow } from "../row"
+
+export const ViewBackupPhraseSetting: React.FC = () => {
+  const { LL } = useI18nContext()
+  const { navigate } = useNavigation<StackNavigationProp<RootStackParamList>>()
+  const { activeAccount } = useAccountRegistry()
+  const { backupState } = useBackupState()
+
+  if (activeAccount?.type !== AccountType.SelfCustodial) return null
+  if (backupState.status !== BackupStatus.Completed) return null
+
+  return (
+    <SettingsRow
+      title={LL.BackupScreen.ManualBackup.Phrase.headerTitle()}
+      leftGaloyIcon="key-outline"
+      action={() => navigate("sparkBackupAlertsScreen")}
+    />
+  )
+}
