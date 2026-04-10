346b4e00f5cf9cd5facaf7b51a46f379eb9afe5a
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-04-10 11:40:56 -0600

    fix: hide move-to-noncustodial setting when already self-custodial

--- app/screens/settings-screen/settings/account-move-to-noncustodial.tsx
+++ app/screens/settings-screen/settings/account-move-to-noncustodial.tsx
@@ -3,16 +3,21 @@ import React from "react"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
 
+import { useAccountRegistry } from "@app/hooks/use-account-registry"
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { RootStackParamList } from "@app/navigation/stack-param-lists"
 import { useMigrationCheckpoint } from "@app/screens/account-migration/hooks"
+import { AccountType } from "@app/types/wallet.types"
 
 import { SettingsRow } from "../row"
 
 export const MoveToNonCustodialSetting: React.FC = () => {
   const { LL } = useI18nContext()
   const { navigate } = useNavigation<StackNavigationProp<RootStackParamList>>()
+  const { activeAccount } = useAccountRegistry()
   const { loading, getRouteForCheckpoint } = useMigrationCheckpoint()
+
+  if (activeAccount?.type === AccountType.SelfCustodial) return null
 
   return (
     <SettingsRow
