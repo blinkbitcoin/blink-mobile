3bc4b1fa0e4d6d6db2167a8d779d09fc3359d337
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-04-24 19:33:16 -0600

    feat(self-custodial): add SDK error classifier and translator hook

--- app/self-custodial/hooks/index.ts
+++ app/self-custodial/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useNonCustodialConversionLimits } from "./use-non-custodial-conversion-limits"
 export { usePaymentRequest } from "./use-payment-request"
+export { useTranslateSdkError } from "./use-translate-sdk-error"
 export type { SelfCustodialPaymentRequestState, InvoiceData } from "./types"
