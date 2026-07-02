c79b85bacb6d26408c1deb571f9ac5449679704d
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-07-02 13:23:03 -0600

    refactor(card): remove unused top-up screen from the investment flow

--- app/i18n/i18n-types.ts
+++ app/i18n/i18n-types.ts
@@ -12419,30 +12419,6 @@ type RootTranslation = {
 				 */
 				buttonText: string
 			}
-			TopUp: {
-				/**
-				 * T​o​p​-​u​p
-				 */
-				title: string
-				/**
-				 * I​n​s​u​f​f​i​c​i​e​n​t​ ​f​u​n​d​s​ ​i​n​ ​y​o​u​r​ ​B​l​i​n​k​ ​w​a​l​l​e​t​.​
-			​T​o​p​-​u​p​ ​m​i​n​i​m​u​m​ ​o​f​ ​{​m​i​n​A​m​o​u​n​t​}​.
-				 * @param {string} minAmount
-				 */
-				qrTitle: RequiredParams<'minAmount'>
-				/**
-				 * C​o​p​y
-				 */
-				copy: string
-				/**
-				 * S​h​a​r​e
-				 */
-				share: string
-				/**
-				 * S​e​t​ ​a​m​o​u​n​t
-				 */
-				placeholder: string
-			}
 			PersonalInformation: {
 				/**
 				 * F​i​n​a​l​ ​s​t​e​p
@@ -26113,29 +26089,6 @@ export type TranslationFunctions = {
 				 * Deposit
 				 */
 				buttonText: () => LocalizedString
-			}
-			TopUp: {
-				/**
-				 * Top-up
-				 */
-				title: () => LocalizedString
-				/**
-				 * Insufficient funds in your Blink wallet.
-			Top-up minimum of {minAmount}.
-				 */
-				qrTitle: (arg: { minAmount: string }) => LocalizedString
-				/**
-				 * Copy
-				 */
-				copy: () => LocalizedString
-				/**
-				 * Share
-				 */
-				share: () => LocalizedString
-				/**
-				 * Set amount
-				 */
-				placeholder: () => LocalizedString
 			}
 			PersonalInformation: {
 				/**
